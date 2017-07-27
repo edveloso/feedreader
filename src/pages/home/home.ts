54aae68ffcaf9b03316897093c4e2e512a06bf14
edveloso <edveloso@gmail.com>
2017-07-27 07:58:27 -0300

    adicionando refresh

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,10 +12,13 @@ export class HomePage {
 
   public feeds: Array<string>;
   private url: string = "https://www.reddit.com/new.json";
-  private olderPosts: string = "https://www.reddit.com/new.json?after="
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
+  private afterParam: string = "";
+  private beforeParam: string = "";
+  private newerPosts: string = "https://www.reddit.com/new.json?before=";
 
   constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
-  		this.fetchData();  		
+  		this.fetchData();        
   }
 
 
@@ -29,19 +32,19 @@ export class HomePage {
   	this.http.get(this.url).map(res => res.json())
   				.subscribe(data => {
   					this.feeds = data.data.children;
+            this.afterParam = data.data.after;
 
   					loading.dismiss();
             this.feeds.forEach((item, i, a) => {              
-              if(!item['data']['thumbnail'] 
-                  || item['data']['thumbnail'].endsWith('png') !== 1  
+              if(!item['data']['thumbnail']                   
                   || item['data']['thumbnail'].indexOf('b.thumbs.redditmedia.com') !== 1  
                   )   
               {
-                console.log('entrou em ' + i);  
+                
                   item['data']['thumbnail'] = 'http://www.redditstatic.com/icon.png';
               }                     
             });
-            console.log(this.feeds);
+            
   				});
 
 
@@ -51,23 +54,46 @@ export class HomePage {
   	new InAppBrowser(url, "_system");
   }
 
-  doInfinite(infiniteScroll){
-      let paramUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1]['data']['name ']: "";
-      this.http.get(this.olderPosts + paramUrl)
+  doInfinite(infiniteScroll){      
+      this.http.get(this.olderPosts + this.afterParam)
           .map(res => res.json())
           .subscribe(data => {
               this.feeds = this.feeds.concat(data.data.children);
+              this.afterParam = data.data.after;
                this.feeds.forEach((item, i, a) => {              
-              if(!item['data']['thumbnail'] 
-                  || item['data']['thumbnail'].endsWith('png') !== 1  
+              if(!item['data']['thumbnail']                   
                   || item['data']['thumbnail'].indexOf('b.thumbs.redditmedia.com') !== 1  
                   )   
               {
-                console.log('entrou em ' + i);  
+                
                   item['data']['thumbnail'] = 'http://www.redditstatic.com/icon.png';
               }                     
             });
+
                infiniteScroll.complete();
           }); 
   }
+
+doRefresh(refresher) {
+  console.log(refresher);
+    let paramsUrl = this.feeds[0]['data']['name'];
+
+    this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+      
+        this.feeds = data.data.children.concat(this.feeds);
+        
+          this.feeds.forEach((item, i, a) => {              
+              if(!item['data']['thumbnail']                   
+                  || item['data']['thumbnail'].indexOf('b.thumbs.redditmedia.com') !== 1  
+                  )   
+              {
+                
+                  item['data']['thumbnail'] = 'http://www.redditstatic.com/icon.png';
+              }                     
+            })
+        refresher.complete();
+      });
+  } 
+
 }
