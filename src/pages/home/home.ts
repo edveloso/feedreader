ba8595d1670050baf055f333f3c609ac1b79cfc7
edveloso <edveloso@gmail.com>
2017-07-27 06:41:19 -0300

    funcionalidades inicias com consulta a feeds, scroll infinito, inappbrowser e loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { InAppBrowser } from 'ionic-native';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,64 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public feeds: Array<string>;
+  private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after="
+
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+  		this.fetchData();  		
+  }
+
+
+  fetchData():void  {
+  	let loading = this.loadingCtrl.create({
+  		content: 'Buscando os dados'
+  	});
+
+  	loading.present();
+
+  	this.http.get(this.url).map(res => res.json())
+  				.subscribe(data => {
+  					this.feeds = data.data.children;
+
+  					loading.dismiss();
+            this.feeds.forEach((item, i, a) => {              
+              if(!item['data']['thumbnail'] 
+                  || item['data']['thumbnail'].endsWith('png') !== 1  
+                  || item['data']['thumbnail'].indexOf('b.thumbs.redditmedia.com') !== 1  
+                  )   
+              {
+                console.log('entrou em ' + i);  
+                  item['data']['thumbnail'] = 'http://www.redditstatic.com/icon.png';
+              }                     
+            });
+            console.log(this.feeds);
+  				});
+
 
   }
 
+  itemSelecionado(url: string): void {
+  	new InAppBrowser(url, "_system");
+  }
+
+  doInfinite(infiniteScroll){
+      let paramUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1]['data']['name ']: "";
+      this.http.get(this.olderPosts + paramUrl)
+          .map(res => res.json())
+          .subscribe(data => {
+              this.feeds = this.feeds.concat(data.data.children);
+               this.feeds.forEach((item, i, a) => {              
+              if(!item['data']['thumbnail'] 
+                  || item['data']['thumbnail'].endsWith('png') !== 1  
+                  || item['data']['thumbnail'].indexOf('b.thumbs.redditmedia.com') !== 1  
+                  )   
+              {
+                console.log('entrou em ' + i);  
+                  item['data']['thumbnail'] = 'http://www.redditstatic.com/icon.png';
+              }                     
+            });
+               infiniteScroll.complete();
+          }); 
+  }
 }
